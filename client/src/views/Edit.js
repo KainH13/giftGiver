8a355f452ca0671f774021acdef99a9079482f4d
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2022-02-21 15:42:06 -0800

    can save changes to a user profile

--- client/src/views/Edit.js
+++ client/src/views/Edit.js
@@ -1,40 +1,75 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 // components
 import Navbar from "../components/Navbar";
 import UserInfoForm from "../components/UserInfoForm";
 
 const Edit = (props) => {
-    const { userEmail, setUserEmail } = props;
+  const { userEmail, setUserEmail } = props;
 
-    // saving form inputs
-    const [user, setUser] = useState({
-        firstName: "",
-        lastName: "",
-        interests: "",
-        customFields: [],
-    });
-    // validation errors
-    const [errors, setErrors] = useState([]);
+  // saving form inputs
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    interests: "",
+    customFields: [],
+  });
 
-    const navigate = useNavigate();
+  // validation errors
+  const [errors, setErrors] = useState([]);
 
-    useEffect(() => {
-        // check for user authentication via userEmail state
-        if (userEmail === "") {
-            navigate("/login");
-        }
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // check for user authentication via userEmail state
+    if (userEmail === "") {
+      navigate("/login");
+    }
+
+    // populate user data
+    axios
+      .get(`http://localhost:8000/api/users/${userEmail}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setUser({
+          firstName: res.data.firstName,
+          lastName: res.data.lastName,
+          interests: res.data.interests,
+          customFields: res.data.customFields,
+        });
         console.log(user);
-        console.log(user.firstName);
-    });
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  }, []);
 
-    return (
-        <div>
-          <Navbar userEmail={userEmail} setUserEmail={setUserEmail} />
-          <UserInfoForm user={user} setUser={setUser} errors={errors} />
-        </div>
-    );
+  // submit handler that's passed to UserInfoForm
+  const updateUser = (user) => {
+    axios
+      .put(`http://localhost:8000/api/users/${userEmail}`, user, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data);
+        navigate("/home");
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        setErrors(err.response.data.errors);
+      });
+  };
+
+  return (
+    <div>
+      <Navbar userEmail={userEmail} setUserEmail={setUserEmail} />
+      <UserInfoForm user={user} setUser={setUser} onSubmitAction={updateUser} errors={errors} />
+    </div>
+  );
 };
 
 export default Edit;
