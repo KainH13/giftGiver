9bef0818fde75f902cb9e3476ce12c471d0d7345
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2022-04-04 11:14:25 -0700

    routes updated, ready for testing

--- server/routes/request.routes.js
+++ server/routes/request.routes.js
@@ -4,9 +4,31 @@ const { authenticate } = require("../config/jwt.config");
 module.exports = (app) => {
   app.get("/api/requests", authenticate, RequestController.findAllRequests);
   app.post("/api/requests", authenticate, RequestController.createRequest);
-  app.get("/api/user/requests/for", authenticate, RequestController.findAllRequestsForLoggedInUser);
-  app.get("/api/user/requests/by", authenticate, RequestController.findAllRequestsByLoggedInUser);
+  app.get(
+    "/api/user/requests/for",
+    authenticate,
+    RequestController.findAllRequestsForLoggedInUser
+  );
+  app.get(
+    "/api/user/requests/by",
+    authenticate,
+    RequestController.findAllRequestsByLoggedInUser
+  );
   app.get("/api/requests/:id", authenticate, RequestController.findOneRequest);
   app.put("/api/requests/:id", authenticate, RequestController.updateRequest);
-  app.delete("/api/requests/:id", authenticate, RequestController.deleteRequest);
+  app.delete(
+    "/api/requests/:id",
+    authenticate,
+    RequestController.deleteRequest
+  );
+  app.put(
+    "/api/requests/accept",
+    authenticate,
+    RequestController.acceptRequest
+  );
+  app.put(
+    "/api/requests/decline",
+    authenticate,
+    RequestController.declineRequest
+  );
 };
